acdf160de54bd45aa53507a3d19a3da00d03de23
rosemary-crypto <76081961+rosemary-crypto@users.noreply.github.com>
2022-10-20 14:47:05 +0900

    Time: 224 ms (12.06%), Space: 47.7 MB (6.50%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int smallest = 0;
+        std::map<int,int> mapping;
+        for(auto i:nums){
+            mapping[i] = 1;            
+        }
+        int j=1;
+        while(j){
+            if(mapping.find(j)==mapping.end()){
+                smallest = j;
+                break;
+            }
+            j++;
+        }
+        return smallest;
+    }
+};
